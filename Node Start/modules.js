070e0debac33146a1d6d59e850bddeaf362e004f
md5iam <mdsiam9837@gmail.com>
2026-09-14 09:55:05 +0600

    Core Modules and Global Object

---
+++ Node Start/modules.js
@@ -0,0 +1,12 @@
+// const fs = require('fs');
+// const os = require('os');
+const {log} = require('console');
+
+// fs.writeFileSync("dymmy.txt", "This is dummy value")
+
+// console.log(os.platform());
+// console.log(os.hostname());
+// console.log(os.cpus());
+
+console.log(process.cwd());
+log("custome log")
