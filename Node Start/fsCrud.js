28dbba121e23d0712e91b815328098d8074aa1fc
md5iam <mdsiam9837@gmail.com>
2026-09-15 10:39:02 +0600

    CRUD with file system in Node js

---
+++ Node Start/fsCrud.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const operation = process.argv[2];
+
+if ( operation === 'write'){
+  const name = process.argv[3];
+  const content = process.argv[4];
+
+  const fullName = "files/"+name+".txt";
+
+  fs.writeFileSync(fullName, content);
+}
+else if ( operation === 'read'){
+  const name = process.argv[3];
+  const fullName = "files/"+name+".txt";
+
+  let data = fs.readFileSync(fullName, "utf-8");
+  console.log(data);
+}
+else if ( operation === 'update'){
+  const name = process.argv[3];
+  const content = process.argv[4];
+  const fullName = "files/"+name+".txt";
+
+  let data = fs.appendFileSync(fullName, content);
+}
+else if ( operation === 'delete'){
+  const name = process.argv[3];
+  const fullName = "files/"+name+".txt";
+
+  fs.unlinkSync(fullName);
+}
+else{
+  console.log("operation not found");
+}
+
+// fs.writeFileSync("files/apple.txt", "This is apple");
+// fs.unlinkSync("files/banana.txt");
+
+// const data = fs.readFileSync('files/apple.txt', "utf-8");
+// console.log(data);
+
+// fs.appendFileSync("files/apple.txt", "and it is very good")
