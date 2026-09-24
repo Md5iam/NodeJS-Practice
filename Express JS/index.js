4051e943c7647135837cd0709cbf234c40780184
md5iam <mdsiam9837@gmail.com>
2026-09-24 13:30:55 +0600

    How Express JS Works Explained

--- Express JS/index.js
+++ Express JS/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
 const app = express();
-app.get("", (req, res)=>{
-  res.send("<h1> Siam AHMED </h1>")
-});
-app.get("/about", (req, res)=>{
-  res.send("<h1> About Page </h1>")
-});
+
 app.listen(3200);
+
+app.get("", (req , res)=>{
+  res.send("<h1> Siam Ahmed </h1>")
+})
+app.get("/about", (req , res)=>{
+  res.send("<h1> About Page </h1>")
+})
